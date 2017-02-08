6cd0fe1f27bf140ec0e89c6194692554b89f0b0c
VivekParanthaman <vivek.paranthaman@bizruntime.com>
2017-02-08 11:00:01 +0530

    schemaConfig to config

--- metawidget-using-A2_webpack/src/app/metawidget/metawidget.component.ts
+++ metawidget-using-A2_webpack/src/app/metawidget/metawidget.component.ts
@@ -23,7 +23,7 @@ declare var $: any;
 export class MetawidgetComponent implements OnInit {
 
     @Input()
-    schemaConfiguration: Object;
+    config: Object;
     @Input()
     model: any;
     elem: Element;
@@ -39,7 +39,7 @@ export class MetawidgetComponent implements OnInit {
         if( this.model === undefined ) {
             this.model = {};
         }
-        metaWidgetConfiguration(this.elem, this.schemaConfiguration, this, this.vcRef, this.compiler, this.rootComponentReference);
+        metaWidgetConfiguration(this.elem, this.config, this, this.vcRef, this.compiler, this.rootComponentReference);
     }
 
     clearWidgets() {
